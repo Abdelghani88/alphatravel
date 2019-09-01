61df28e6259ec11c4884b8fa4e1c73ea8b6a2ff1
Abdelghani Ait Souiri <abdelghani.ait.souiri@gmail.com>
2019-09-01 16:55:10 -0500

    initial commit

---
+++ src/main/java/com/souirix/alphatravel/AlphatravelApplication.java
@@ -0,0 +1,13 @@
+package com.souirix.alphatravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphatravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphatravelApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/souirix/alphatravel/Controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.souirix.alphatravel.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value=())
+    public String Hello(){
+        return "home";
+    }
+}
